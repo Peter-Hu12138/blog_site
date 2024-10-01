e8d9e80a89f878831c9dbe54e8af95e595040a68
Peter-Hu12138 <hjtwillbeok@gmail.com>
2024-10-01 01:09:02 -0400

    Minor spacing fixes

--- comments_and_user_registration/urls.py
+++ comments_and_user_registration/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import include, path
+from members import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('members.urls')),
     path('', include('account.urls')),
-    path('', views.main, name='main'),
+    path('', views.main, name='home'),
     path('testing/', views.testing, name='testing'),
-    path('user_registration/', views.user_registration, name='user_registration'),
-    path('user_registration/thanks/', views.thanks, name='thanks'),
-
+    # replaced by account/signup
+    # path('user_registration/', views.user_registration, name='user_registration'),
+    # path('user_registration/thanks/', views.thanks, name='thanks'),
 ]
