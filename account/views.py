5e63e6176223b626d1539869eafb62cb64a687e7
Peter-Hu12138 <hjtwillbeok@gmail.com>
2024-10-01 01:04:33 -0400

    1. transform and change user_registration, the view, from members/views.py to account/views.py for better organization 2.

--- account/views.py
+++ account/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.template import loader
+from django.contrib.auth.models import User
+from .forms import user_form
 
 # Create your views here.
+# TODO: catch the unique user name error with proper UI
+def sign_up(request: HttpRequest):
+    """
+    Register a user and redirect to homepageif a form is valid; otherwise, direct the user to fill the form.
+    """
+    if request.method == "POST":
+        form = user_form(request.POST)
 
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['user_name'],
+                                            email=form.cleaned_data['email'],
+                                            password=form.cleaned_data['password'])
+            return HttpResponseRedirect("login")
+
+    else:
+        form = user_form()
+
+    return render(request, "registration/signup.html", {"form": form})
--- members/views.py
+++ members/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest
 from django.template import loader
 from .models import Member
-from .forms import user_form
 
 
 # This was mostly an archive for early exploration
 
 def members(request: HttpRequest):
     mymembers = Member.objects.all().values()
-    template = loader.get_template("memberlist.html")
     context = {
         'mymembers': mymembers,
     }
@@ -28,7 +26,7 @@ def details(request, id):
 
 def main(request):
     template = loader.get_template('main.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render({}, request))
 
 
 def testing(request):
@@ -48,23 +46,5 @@ def detailed_member_inventory(request: HttpRequest):
     return HttpResponse(template.render(context, request))
 
 
-def user_registration(request: HttpRequest):
-    if request.method == "POST":
-        form = user_form(request.POST)
-        if form.is_valid():
-            # TODO: try this with return HttpResponseRedirect("/thanks/")
-            new_member = Member(firstname=form.cleaned_data['firstname'], lastname=form.cleaned_data['lastname'],
-                                age=form.cleaned_data['age'], phone=form.cleaned_data['phone'])
-            new_member.save()
-            return HttpResponseRedirect("/user_registration/thanks/")
-            # return render(HttpRequest(), "thanks.html", {})
-
-    else:
-        form = user_form()
-
-    return render(request, "registration/user_registration.html", {"form": form})
-
 def thanks(request):
     return render(request, 'thanks.html')
-
-
