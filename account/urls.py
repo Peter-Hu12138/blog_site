43a1801724cf25defadee2c90bfbaf0b673ea60e
Peter-Hu12138 <hjtwillbeok@gmail.com>
2024-10-01 01:08:11 -0400

    delete url paths for user_registration in members/urls.py in exchange for account/signup; refactoring for clear structure

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+# account/login is directed to login.html by "auth"
 urlpatterns = [
     # path('account/', views.members, name='members'),
     path("account/", include("django.contrib.auth.urls")),  # default is to look templates in "templates" in an app?
+    path("account/signup", views.sign_up, name="signup")
 ]
--- members/urls.py
+++ members/urls.py
@@ -1,13 +1,11 @@
 """
 all urls affiliated with members
 """
-from django.urls import path, include
+from django.urls import path
 from . import views
-
 
 urlpatterns = [
     path('members/', views.members, name='members'),
-
     path('members/inventory', views.detailed_member_inventory, name='member_inventory'),
     path('members/details/<int:id>', views.details, name='details'),
 ]
