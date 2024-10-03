c0741a99af311773a08a277b3ca14312e71bd6c4
Peter-Hu12138 <hjtwillbeok@gmail.com>
2024-10-03 15:38:16 -0400

    add profile url to urls.py

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 # account/login is directed to login.html by "auth"
 urlpatterns = [
     # path('account/', views.members, name='members'),
+    path("account/signup", views.sign_up, name="signup"),
+    path("account/profile", views.profile, name="profile"),
     path("account/", include("django.contrib.auth.urls")),  # default is to look templates in "templates" in an app?
-    path("account/signup", views.sign_up, name="signup")
 ]
